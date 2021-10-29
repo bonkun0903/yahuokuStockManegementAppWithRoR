5e3bad89f335b52047dd86ddd098d5728ee46d33
hiiiroaqua <plutonium.090301@gmail.com>
2021-10-29 10:47:35 +0900

    Change hello_vue.js to app.js

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'homes/index'
   namespace :api, { format: "json" } do
     namespace :v1 do
       resources :todos, :only => [:index, :show, :create, :update, :destroy]
